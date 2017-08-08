52db64b7d448e98e5769c843428f4438e26c03f5
Evan <gthrowaway28@gmail.com>
2017-08-08 05:14:13 +0000

    assigned layout renders to all screen to ensure no conflicts | fixed navbar collapse

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,12 @@ class StaticPagesController < ApplicationController
     def home
         render layout: "homelayout"
     end
-    
+
     def development
+        render layout: "application"
     end
     
     def design
+        render layout: "application"
     end
 end
