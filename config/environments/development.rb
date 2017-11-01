b4ac6f2e10c56f787c269ad2f73fdb029c9256f0
Evan <gthrowaway28@gmail.com>
2017-11-01 08:44:24 +0000

    fixed error in controller to utilize email params. Removed some hacked code for better security practices. Correct flash messages showing up as well. Working locally and at heroku. WOO!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,15 +33,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                      587,
-    domain:                 'gmail.com',
-    user_name:        "evanbogartportfolio@gmail.com",
-    password:       "faljkdlpsqr32",
-    authentication:      'plain',
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
     enable_starttls_auto: true,
-    :openssl_verify_mode  => 'none'
-    }
+    :openssl_verify_mode  => 'none',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
